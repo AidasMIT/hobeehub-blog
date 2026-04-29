1c9d1575baac1d5b92575b4c11d6870ec0ea88c6
Rikis <rikis@unchainit.tech>
2026-04-29 13:56:02 +0000

    Initial HobeeHub blog setup

---
+++ src/consts.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'HobeeHub Blog';
+export const SITE_DESCRIPTION = 'Straipsniai apie pomėgius, veiklas, aktyvų gyvenimą ir bendruomenę Lietuvoje. Atrask naujus pomėgius su HobeeHub.';
+export const SITE_URL = 'https://blog.hobeehub.com';
